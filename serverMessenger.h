059ba11db46f65a8c19f5b055407462f82121222
Brett <bmgeorg@g.clemson.edu>
2015-04-19 16:12:32 -0400

    Add serverMessenger; update Makefile

---
+++ serverMessenger.h
@@ -0,0 +1,14 @@
+#ifndef SERVER_MESSENGER_H
+#define SERVER_MESSENGER_H
+
+/*
+	serverMessenger handles all communication with robotClient. 
+*/
+
+extern const int RESPONSE_MESSAGE_SIZE;
+
+//public functions
+//Send response, chunking into RESPONSE_MESSAGE_SIZE-sized chunks
+void sendResponse(char* response, int responseLength, int requestID);
+
+#endif
