b25bd9f497d8b2b62bffdc88cff800a12c0f1cf6
Brett <bmgeorg@g.clemson.edu>
2015-04-22 10:38:13 -0400

    Refactor server.c

--- server.c
+++ server.c
@@ -19,65 +19,48 @@
 
 //Method Signatures
 char* getRobotID(char* msg);
-uint32_t getReqID(char* msg);
-char* getReq(char* msg);
-char* generateHTTPReq(char* robotAddress, char* robotID, char* reqStr, char* imageID);
-char* getPort(char* robotCommand);
+uint32_t getRequestID(char* msg);
+char* getRequestStr(char* msg);
+char* generateHTTPRequest(char* robotAddress, char* robotID, char* requestStr, char* imageID);
+char* getRobotPortForRequestStr(char* requestStr);
 void flushBuffersAndExit();
 
 //Main Method
 int main(int argc, char *argv[])
 {
-	if (argc != 5) {
+	if(argc != 5) {
 		fprintf(stderr,"Usage:  %s <server_port> <robot_IP/robot_hostname> <robot_ID> <image_id>\n", argv[0]);
 		exit(1);
 	}
+	//read args
+	unsigned short localUDPPort = atoi(argv[1]);
+	char* robotAddress = argv[2];
+	char* robotID = argv[3];
+	char* imageID = argv[4];
 	
 	//listen for ctrl-c and call flushBuffersAndExit()
 	signal(SIGINT, flushBuffersAndExit);
-	
-	unsigned short localUDPPort;	/* Server port */
-	
-	//Robot Variables
-	char* robotAddress;
-	char* robotID;
-	char* imageID;
-	uint32_t reqID;
-	char* reqStr;
-	char* request;
 
-	//TCP Socket Variables
-	int sockRobot;
-	char* responseBuffTCP;
-	responseBuffTCP = malloc(sizeof(char)*1000);
-
-	localUDPPort = atoi(argv[1]); 
-	robotAddress = argv[2];
-	robotID = argv[3];
-	imageID = argv[4];
-	
-	
 	//Create socket for talking to clients
 	int clientSock;
-	if ((clientSock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
-		quit("socket() failed\n");
+	if((clientSock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+		quit("could not create client socket - socket() failed");
 	}
 		
-	//Construct local address structure
+	//Construct local address structure for talking to clients
 	struct sockaddr_in localAddress;
 	memset(&localAddress, 0, sizeof(localAddress));		//Zero out structure
 	localAddress.sin_family = AF_INET;					//Any Internet address family
 	localAddress.sin_addr.s_addr = htonl(INADDR_ANY);	//Any incoming interface
 	localAddress.sin_port = htons(localUDPPort);		//The port clients will sendto
 
-	//Bind to the local address
-	if (bind(clientSock, (struct sockaddr *) &localAddress, sizeof(localAddress)) < 0) {
-		quit("bind() failed\n");
+	if(bind(clientSock, (struct sockaddr *) &localAddress, sizeof(localAddress)) < 0) {
+		quit("could not bind to client socket - bind() failed");
 	}
 
-	//HANDLE UDP CLIENT
-	//Run forever
-	for (;;) {
+	//Loop for each client request
+	for(;;) {
+		//Receive request from client
 		struct sockaddr_in clientAddress;
 		unsigned int clientAddressLen = sizeof(clientAddress);	//in-out parameter
 		char clientBuffer[MAXLINE+1];	//Buffer for incoming client requests
@@ -85,144 +68,140 @@ int main(int argc, char *argv[])
 		
 		int recvMsgSize;
 		//Block until receive a guess from a client
-		if ((recvMsgSize = recvfrom(clientSock, clientBuffer, MAXLINE, 0,
+		if((recvMsgSize = recvfrom(clientSock, clientBuffer, MAXLINE, 0,
 				(struct sockaddr *) &clientAddress, &clientAddressLen)) < 0) {
-			quit("recvfrom() failed\n");
+			quit("could not receive client request - recvfrom() failed");
 		}
 
-		//Interpret Clients Request
-		reqID = getReqID(clientBuffer);
-		if(strcmp(robotID, getRobotID(clientBuffer)) != 0) {
-			fprintf(stderr, "Robot ID's don't match\n");
+		//Interpret client request
+		char* requestRobotID = getRobotID(clientBuffer);
+		if(strcmp(robotID, requestRobotID) != 0) {
+			fprintf(stderr, "invalid request - robot ID's don't match\n");
 			continue;
 		}
 
-		reqStr = NULL;
-		reqStr = getReq(clientBuffer);
-		
-		//SEND HTTP GET REQUEST
-		
-		sockRobot = setupSocket(robotAddress, getPort(reqStr), TCP);
-	
-		//Send HTTP Req. to Robot
-		request = generateHTTPReq(robotAddress, robotID, reqStr, imageID);
-		if(write(sockRobot, request, strlen(request)) != strlen(request)) {
-			fprintf(stderr, "Write error.\n");
-			continue;
+		char* requestStr = getRequestStr(clientBuffer);
+		char* robotPort = getRobotPortForRequestStr(requestStr);
+		//Send HTTP request to robot
+		int robotSock;
+		if((robotSock = setupSocket(robotAddress, robotPort, TCP)) < 0) {
+			quit("could not connect to robot");
+		}	
+		char* httpRequest = generateHTTPRequest(robotAddress, robotID, requestStr, imageID);
+		if(write(robotSock, httpRequest, strlen(httpRequest)) != strlen(httpRequest)) {
+			quit("could not send http request to robot - write() failed");
 		}
 
 		//Read response from Robot
-		int n;
 		int pos = 0;
-		char recvline[MAXLINE+1];
-		while( (n = read(sockRobot, recvline, MAXLINE)) > 0) {
-			printf("loop");
-			fflush(stdout);
-			memcpy(responseBuffTCP+pos, recvline, n);
+		char* httpResponse = malloc(MAXLINE);
+		int n;
+		char recvLine[MAXLINE+1]; //holds one chunk of read data at a time
+		while((n = read(robotSock, recvLine, MAXLINE)) > 0) {
+			memcpy(httpResponse+pos, recvLine, n);
 			pos += n;
-			responseBuffTCP = realloc(responseBuffTCP, pos+MAXLINE);
-			printf("postloop");
-			fflush(stdout);
+			httpResponse = realloc(httpResponse, pos+MAXLINE);
 		}
-		printf("Response:\n%s", responseBuffTCP);
+		printf("Response:\n%s", httpResponse);
 		
 		//Parse Response from Robot
-		char* end = strstr(responseBuffTCP, "\r\n\r\n")+4;
-		int responseLength = (responseBuffTCP+pos)-end;
-		printf("Response length: %d\n", responseLength);
-		/* Send response back to the UDP client */
-		sendResponse(clientSock, &clientAddress, clientAddressLen, reqID, end, responseLength);
+		char* httpBody = strstr(httpResponse, "\r\n\r\n")+4;
+		int httpBodyLength = (httpResponse+pos)-httpBody;
+		printf("Response length: %d\n", httpBodyLength);
+		
+		//Send response back to the UDP client
+		uint32_t requestID = getRequestID(clientBuffer);
+		sendResponse(clientSock, &clientAddress, clientAddressLen, requestID, httpBody, httpBodyLength);
+		
+		free(httpResponse);
 	}
-	
-	free(responseBuffTCP);
-	free(request);
 }
 
 char* getRobotID(char* msg) {
 	return msg+4;
 }
 
-uint32_t getReqID(char* msg) {
+uint32_t getRequestID(char* msg) {
 	return ntohl(*((uint32_t*)msg));
 }
 
-char* getReq(char* msg) {
+char* getRequestStr(char* msg) {
 	char* robotID = getRobotID(msg);
 
 	return msg+5+strlen(robotID);
 }
 
-char* generateHTTPReq(char* robotAddress, char* robotID, char* reqStr, char* imageID) {
-	char* URI;
-	URI = (char*) malloc(sizeof(char)*MAXLINE);
-	char* ptr;
-	int n = 0;
-	if(strstr(reqStr, "MOVE") != NULL) {
-		ptr = strpbrk(reqStr, "0123456789");
-		n = atoi(ptr);
-		sprintf(URI, "/twist?id=%s&lx=%d", robotID, n);
-	} else if(strstr(reqStr, "TURN") != NULL) {
-		ptr = strpbrk(reqStr, "0123456789");
-		n = atoi(ptr);
-		sprintf(URI, "/twist?id=%s&az=%d",robotID,n);
-	} else if(strstr(reqStr, "STOP") != NULL) {
+char* generateHTTPRequest(char* robotAddress, char* robotID, char* requestStr, char* imageID) {
+	char* URI = malloc(MAXLINE);
+	double x;
+	if(sscanf(requestStr, "MOVE %lf", &x) == 1) {
+		sprintf(URI, "/twist?id=%s&lx=%lf", robotID, x);
+	}
+	else if(sscanf(requestStr, "TURN %lf", &x) == 1) {
+		sprintf(URI, "/twist?id=%s&az=%lf", robotID, x);
+	}
+	else if(strstr(requestStr, "STOP") != NULL) {
 		sprintf(URI, "/twist?id=%s&lx=0",robotID);
-	} else if(strstr(reqStr, "GET IMAGE") != NULL) {
+	}
+	else if(strstr(requestStr, "GET IMAGE") != NULL) {
 		sprintf(URI, "/snapshot?topic=/robot_%s/image?width=600?height=500", imageID);
-	} else if(strstr(reqStr, "GET GPS") != NULL) {
+	}
+	else if(strstr(requestStr, "GET GPS") != NULL) {
 		sprintf(URI, "/state?id=%s",robotID);
-	} else if(strstr(reqStr, "GET DGPS") != NULL) {
+	}
+	else if(strstr(requestStr, "GET DGPS") != NULL) {
 		sprintf(URI, "/state?id=%s",robotID);
-	} else if(strstr(reqStr, "GET LASERS") != NULL) {
+	}
+	else if(strstr(requestStr, "GET LASERS") != NULL) {
 		sprintf(URI, "/state?id=%s",robotID);
-	} else {
-		sprintf(URI, "invalid");
+	}
+	else {
+		free(URI);
+		return NULL;
 	}
 
-	char* req = malloc(sizeof(char)*MAXLINE);
-	strcat(req, "GET ");
-	strcat(req, URI);
-	strcat(req, " HTTP/1.1\r\n");
+	char* httpRequest = malloc(MAXLINE);
+	strcat(httpRequest, "GET ");
+	strcat(httpRequest, URI);
+	strcat(httpRequest, " HTTP/1.1\r\n");
 	
 	//Host
-	strcat(req, "Host: ");
-	strcat(req, robotAddress);
-	strcat(req, ":");
-	strcat(req, getPort(reqStr));
-	strcat(req, "\r\n");
+	strcat(httpRequest, "Host: ");
+	strcat(httpRequest, robotAddress);
+	strcat(httpRequest, ":");
+	strcat(httpRequest, getRobotPortForRequestStr(requestStr));
+	strcat(httpRequest, "\r\n");
 	
 	//Connection: close
-	strcat(req, "Connection: close\r\n");
+	strcat(httpRequest, "Connection: close\r\n");
 	
-	strcat(req, "\r\n");
+	strcat(httpRequest, "\r\n");
 	
-	fprintf(stderr, "Request:\n%s", req);	
+	fprintf(stderr, "HTTP Request:\n%s", httpRequest);	
 
 	free(URI);
-	return req;
+	return httpRequest;
 }
 
 //Gets TCP Port
-char* getPort(char* reqStr) {
-	
-	if(strstr(reqStr, "MOVE") != NULL) {
+char* getRobotPortForRequestStr(char* requestStr) {
+	if(strstr(requestStr, "MOVE") != NULL) {
 		return "8082";
-	} else if(strstr(reqStr, "TURN") != NULL) {
+	} else if(strstr(requestStr, "TURN") != NULL) {
 		return "8082";
-	} else if(strstr(reqStr, "STOP") != NULL) {
+	} else if(strstr(requestStr, "STOP") != NULL) {
 		return "8082";
-	} else if(strstr(reqStr, "GET IMAGE") != NULL) {
+	} else if(strstr(requestStr, "GET IMAGE") != NULL) {
 		return "8081";
-	} else if(strstr(reqStr, "GET GPS") != NULL) {
+	} else if(strstr(requestStr, "GET GPS") != NULL) {
 		return "8082";
-	} else if(strstr(reqStr, "GET DGPS") != NULL) {
+	} else if(strstr(requestStr, "GET DGPS") != NULL) {
 		return "8084";
-	} else if(strstr(reqStr, "GET LASERS") != NULL) {
+	} else if(strstr(requestStr, "GET LASERS") != NULL) {
 		return "8083";
 	} else {
-		return "0";
+		return NULL;
 	}
-	
 }
 
 /* This routine contains the data printing that must occur before the program 
